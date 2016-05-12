e8ff0094db4e3976ead9f70b0b7771dfd4136948
Victor Nicolet <victor.nicolet.0@gmail.com>
2016-05-12 14:54:56 -0400

    Examples 1 : simple array subscripts

---
+++ synth/utils.rkt
@@ -0,0 +1,10 @@
+#lang rosette/safe
+
+
+(provide PolytopeLU)
+;; Generates the assertions for a polytope P such that each  a < u and a <=
+;; (PolytopeLU [lower bounds ..] [indexes ... ] [upper bounds ...])
+(define-syntax PolytopeLU
+  (syntax-rules ()
+    [(PolytopeLU [l ...] [a ...] [u ...]) 
+     (assert (and (and (< a u) (<= l a)) ...)) ]))
