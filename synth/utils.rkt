52e43a3d6d920e7a9a89d3a072ca6156e976d352
Victor Nicolet <victor.nicolet.0@gmail.com>
2016-05-13 14:21:30 -0400

    Added arithmetic expressions

--- synth/utils.rkt
+++ synth/utils.rkt
@@ -1,8 +1,9 @@
 #lang rosette/safe
 
+(require rosette/lib/synthax)
 
 (provide PolytopeLU get-synthax
-)
+         ArithOp ArithExpr)
 ;; Generates the assertions for a polytope P such that each  a < u and a <=
 ;; (PolytopeLU [lower bounds ..] [indexes ... ] [upper bounds ...])
 (define-syntax PolytopeLU
@@ -12,3 +13,21 @@
 
 (define (get-synthax synthesis-solution) 
   (car synthesis-solution))
+
+;; Synthesizable arithmetic expressions, based onyl on Rosette symbolic values
+;; ArithExpr c ... can synthesize any arithmetic expression involving ArithOp
+;; binary operators, and constants and identifiers in c...
+
+(define-synthax ArithOp
+  ([(ArithOp) (choose + * - /)]))
+
+(define-synthax (ArithExpr c ... depth)
+  #:base (choose c ... (??))
+  #:else (choose
+          ;; Scalar value
+          (c ... (??))
+          ;; Binary operator 
+          ((ArithOp)
+           (ArithExpr c ... (??) (sub1 depth))
+           (ArithExpr c ... (??) (sub1 depth)))
+))
