5f51c03de59f0e17e0715faf3e22a28578147d55
Victor Nicolet <victor.nicolet.0@gmail.com>
2016-06-21 18:37:27 -0400

    Wrapping the loop in a module

--- consynth/canalysis/src/Loops2ssa.ml
+++ consynth/canalysis/src/Loops2ssa.ml
@@ -4,23 +4,17 @@ open Loops
 open Printf
 
 module IH = Inthash
+module VS = Utils.VS
 
 let debug = ref false
 (** The loops in the files *)
 let loops = ref (IH.create 10)
 
-type guard =
-    GEmpty 
-  | GCond of Cil.exp * guard
-  | GFor of Loops.forIGU * guard
 
-
-let rec gCompose g1 g2 =
-  match g1 with
-  | GEmpty -> g2
-  | GCond (e, g) -> GCond (e, gCompose g g2)
-  | GFor (igu, g) -> GFor (igu, gCompose g g2)
-
+(** ------------------------------------------------------------------*)
+(** Type for intermediary representation of the loop body *)
+(** The loop body is a Inthash from the state variable ids 
+    to an expression of type preFunc *)
 (** Short functional representation *)
 type preFunc = 
   | Empty
@@ -34,9 +28,30 @@ type preFunc =
       expression in the assignment.
       The subscript expression list is empty if it is a scalar.
   *)
+  (** init, igu, subscripts, expression *)
   | ForLoop of preFunc * Loops.forIGU * Cil.exp list * preFunc
   (* An expression guarded by an if *)
   | Guarded of preFunc * preFunc * preFunc
+
+(** 
+    Some types to record the control statements above 
+    an expression
+ *)
+type guard =
+    GEmpty 
+  | GCond of Cil.exp * guard
+  | GFor of Loops.forIGU * guard
+
+
+(** ------------------------------------------------------------------*)
+(** Utilities for the types *)
+
+let rec gCompose g1 g2 =
+  match g1 with
+  | GEmpty -> g2
+  | GCond (e, g) -> GCond (e, gCompose g g2)
+  | GFor (igu, g) -> GFor (igu, gCompose g g2)
+
 
 let rec ps_pf pf =
   match pf with
@@ -48,7 +63,7 @@ let rec ps_pf pf =
      String.concat " " [(psprint80 Cil.d_unop op); (ps_pf e)]
   | ForLoop (e0, (i, g, u), el, e) ->
      String.concat " "  ([ "Init: "^(ps_pf e0)^"\nFor {"; 
-                           (psprint80 Cil.d_instr i); 
+                           (psprint80 Cil.d_instr i);
                            (psprint80 Cil.d_exp g); 
                            (psprint80 Cil.d_instr u);
                            "[:"]@
@@ -162,7 +177,7 @@ and do_s hm g stm =
   | _ -> ()
 
 
-let ssaify stmtlist statevars =
+let prefunc stmtlist statevars =
   if !debug then print_endline "---Transform to SSA---";
   let assignments = IH.create 10 in
   List.iter (fun s -> do_s assignments GEmpty s) stmtlist;
@@ -170,8 +185,16 @@ let ssaify stmtlist statevars =
     begin
       IH.iter (fun i v -> printf "%i = %s\n" i (ps_pf v)) assignments;
       printf "-----------\n"
-    end
+    end;
+  assignments
       
+(** ------------------------------------------------------------------*)
+(** 
+    Control-flow graph operations to remove the statements updating
+    the loop index and the break statement.
+    Another solution would be to patch Cil so it keeps the structure of the
+    for loops and doesn't translate them to this while(1) structure.
+*)
 
 let removeFromCFG (stm : Cil.stmt) =
   let succs = stm.succs in
@@ -179,10 +202,7 @@ let removeFromCFG (stm : Cil.stmt) =
   let eq_stm s = (stm.sid = s.sid) in
   List.iter (fun s -> (s.succs <- List.filter eq_stm s.succs)) preds;
   List.iter (fun s -> (s.preds <- List.filter eq_stm s.preds)) succs
-(**
-   We cannot convert the Cil body of the loop directly, we want to remove the
-   effect on the loop index.
-*)
+
 let removeIGU (whileStmt : Cil.stmt) ((i, g, u) : forIGU) =
   match whileStmt.skind with
   | Loop (blk, loc, so1, so2) ->
@@ -216,32 +236,72 @@ let removeIGU (whileStmt : Cil.stmt) ((i, g, u) : forIGU) =
 	blk.bstmts <- stmts
   | _ -> raise (Failure "Expected a loop statement in removeIGU")
 
+(** ------------------------------------------------------------------*)
+(** 
+    main interface functions. From the result of using the Loops module to
+    compute some information, deduce a semi-functional representation
+    of the loop body. 
+*)
 
-let processLoop (sid : int) (cl : Cloop.t) =
-  if !debug then Printf.printf "--- Loop %i --> SSA ---\n" sid;
-  let stateVars = Utils.outer_join_lists
-	(match cl.Cloop.rwset with (r, w, rw) -> w, rw) in
-(**  let loopIndex = indexOfIGU  (checkOption cl.Cloop.loopIGU) in *)
-  let loop_stmt = cl.Cloop.loopStatement in
-  removeIGU loop_stmt (checkOption cl.Cloop.loopIGU);
-  if !debug then 
-    begin
-      Printf.printf "Loop after removing IGU:\n";
-      pps cl.Cloop.loopStatement;
-    end;
-  let body_stmts = 
-    match loop_stmt.skind with 
-    | Loop (blk, _, _, _) -> blk.bstmts
-    | _ -> raise (Failure "processLoop : this should be a loop statement.")
-  in
-  ssaify body_stmts stateVars
-  
 
+module Floop = struct
+    type t = {
+      sid: int;
+      mutable igu: forIGU;
+      mutable body : preFunc IH.t;
+      mutable state : int list;
+      mutable parentLoops : int list;
+      mutable definedInVars: defsMap;
+      mutable usedOutVars: varinfo list;
+      mutable allVars: VS.t ;
+    }
+
+    let fromCloop (sid: int) (cl : Cloop.t) =
+      if !debug then Printf.printf "--- Loop %i --> SSA ---\n" sid;
+      let stateVars = Utils.outer_join_lists
+	    (match cl.Cloop.rwset with (r, w, rw) -> w, rw) in
+  (**  let loopIndex = indexOfIGU  (checkOption cl.Cloop.loopIGU) in *)
+      let loop_stmt = cl.Cloop.loopStatement in
+      let loop_igu = checkOption cl.Cloop.loopIGU in 
+      removeIGU loop_stmt loop_igu;
+      if !debug then 
+        begin
+          Printf.printf "Loop after removing IGU:\n";
+          pps cl.Cloop.loopStatement;
+        end;
+      let body_stmts = 
+
+        match loop_stmt.skind with 
+        | Loop (blk, _, _, _) -> blk.bstmts
+        | _ -> raise (Failure "processLoop : this should be a loop statement.")
+      in
+      {
+        sid = sid;
+        igu = loop_igu;
+        body = prefunc body_stmts stateVars;
+        state = stateVars;
+        parentLoops = cl.Cloop.parentLoops;
+        definedInVars = cl.Cloop.definedInVars;
+        usedOutVars = cl.Cloop.usedOutVars;
+        allVars = VS.empty;
+      }
+end
 
 (** Main entry point *)
 
 let processFile_l2s lps =
   loops := lps;
-  IH.iter processLoop lps
-
+  let floops = IH.create 10 in
+  begin
+    IH.iter (fun sid cloop ->
+      if cloop.Cloop.hasBreaks 
+      then () 
+      else
+        begin
+          let floop = Floop.fromCloop sid cloop in
+          IH.add floops sid floop
+        end) 
+      lps
+  end;
+  floops
 
