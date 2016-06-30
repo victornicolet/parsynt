0167ec5c739115270d6bc6748744aae6cc15b3e9
Victor Nicolet <victor.nicolet.0@gmail.com>
2016-06-30 18:28:33 -0400

    Operators

--- consynth/canalysis/src/RosetteTypes.ml
+++ consynth/canalysis/src/RosetteTypes.ml
@@ -1,15 +1,6 @@
 open Cil
 open Utils
 
-(*
-  Booleans
-  boolean?, false?, true, false, not, nand, nor, implies, xor
-  Integers and Reals
-  number?, real?, integer?, zero?, positive?, negative?, even?, odd?,
-  inexact->exact, exact->inexact, +, -, *, /, quotient, remainder
-  , modulo, add1, sub1, abs, max, min, floor, ceiling, truncate,
-  =, <, <=, >, >=, expt, pi, sgn
-*)
 
 type symbolicType =
   | Unit
@@ -78,3 +69,60 @@ and symb_type_of_args argslisto =
     | [st] -> st
     | _ -> Tuple symb_types_list
   with Failure s -> Unit
+
+
+(*
+  Booleans
+  boolean?, false?, true, false, not, nand, nor, implies, xor
+  Integers and Reals
+  number?, real?, integer?, zero?, positive?, negative?, even?, odd?,
+  inexact->exact, exact->inexact, +, -, *, /, quotient, remainder
+  , modulo, add1, sub1, abs, max, min, floor, ceiling, truncate,
+  =, <, <=, >, >=, expt, pi, sgn
+*)
+
+type symbUnops =
+  | Not | Add1 | Sub1 | Abs | Floor | Ceiling | Truncate
+  | Neg
+  (** Misc*)
+  | Expt | Sgn
+
+type symbBinops =
+  (** Booleans*)
+  | And | Nand | Or | Nor | Implies | Xor
+  (** Integers and reals *)
+  | Plus | Minus | Times | Div | Quot | Rem | Mod
+  | Max | Min
+  (** Comparison *)
+  | Eq | Lt | Le | Gt | Ge | Ne
+  (** Shift*)
+  | ShiftL | ShiftR
+
+let symb_unop_of_cil =
+  function
+  | LNot | BNot -> Not
+  | Neg -> Neg
+
+let symb_binop_of_cil =
+  function
+  | IndexPI -> Plus
+  | PlusA | PlusPI -> Plus
+  | MinusA | MinusPI | MinusPP-> Minus
+  | Mult -> Times
+  | Div -> Div
+  | Mod -> Mod
+  | BXor -> Xor
+  | BAnd | LAnd -> And
+  | BOr | LOr -> Or
+  | Lt -> Lt | Le -> Le | Gt -> Gt | Ge -> Ge
+  | Eq -> Eq | Ne -> Ne
+  | Shiftlt -> ShiftL | Shiftrt -> ShiftR
+
+(**
+    TODO simple recognition of the functions imeplementing the
+    operators described above.
+*)
+
+let symb_binop_of_fname =
+  function
+  |
