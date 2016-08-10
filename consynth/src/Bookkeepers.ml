86bdbfffc83a478c757ee32e9173c7d76bc9caac
Victor Nicolet <victor.nicolet.0@gmail.com>
2016-08-10 18:32:23 -0400

    WIP : loop normalization iwth innit/guard/update inspection

--- consynth/src/Bookkeepers.ml
+++ consynth/src/Bookkeepers.ml
@@ -1,7 +1,9 @@
 open Cil2Func
 open Format
 open Utils
-open Loops
+open Cil
+open Findloops
+open VariableAnalysis
 
 (* let insert_write_flags (letform : letin) : letin = *)
 (*   let iwf_core letf = *)
@@ -11,10 +13,40 @@ open Loops
 (*     | _ -> letf *)
 (*   in *)
 (*   transform_topdown iwf_core letform *)
+type igu = Cil.varinfo * Cil.exp * Cil.exp * Cil.exp
 
-let convert_igu (i, g, u) =
-  let i = indexOfIGU (i, g, u) in
+let convert_igu (i, g, u) reaching_consts =
+  let indexes = index_of_igu (i, g, u) in
   (** Easy case : there is only one index *)
+  if VS.cardinal indexes = 1
+  then
+    let index = VS.max_elt indexes in
+    let init_val =
+      try
+        (IM.find index.vid reaching_consts)
+      with Not_found ->
+        (match i with
+        | Set (vi, Const c, t) -> c
+        | _ -> failwith "Unrecognized statement in init loop")
+    in
+    let increment =
+      match u with
+      | Set ((h,o), BinOp(op, e1, e2, t), t) ->
+         (match e1, e2 with
+         | Lval (h', _), e | e, Lval (h', _) ->
+            (let vi', _ = analyze_host h' in
+             let vi , _ = analyze_host h in
+             if vi = vi' then
+               e
+             else
+               failwith "TODO")
+         | _, _ -> )
+      | Set (vi, UnOp(op, e, t), t) ->
+         ()
+    in
+    (index, init_val, g, increment)
+
+  else ()
   (**
       Initialization of i :
 
