51b75a45be1e61b6e506fdde1e5249935b817aeb
Victor Nicolet <victor.nicolet.0@gmail.com>
2016-08-23 13:53:08 -0400

    Update README, remove old tests

--- tests/frontc/test-cfg.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-(require c "../../consynth/frontc/cfg.rkt"
-         "../../consynth/frontc/pprint.rkt")
-
-(define program (parse-program (open-input-file "test_cfg_all_constructs.c")))
-
-(print-cfg (car (compute-cfg program)))
-
