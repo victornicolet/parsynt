66c3fc8b1017eca9dd3173e188ac7e4fc1f5e175
Victor Nicolet <victor.nicolet@polytechnique.edu>
2016-11-12 00:34:46 +0100

    Simple script for experiments

--- examples/tbb/run.sh
+++ examples/tbb/run.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-EX_SIZE=20
-while [  $EX_SIZE -lt 31 ]; do
-    #Run the experiment with num_core 0 to get the sequential time
-    eval "./tbb_test -n0 -e$EX_SIZE"
-    #Run the experiment with 1 to 64 cores
-    NUM_CORE=1
-    while [  $NUM_CORE -lt 65 ]; do
+
+for EX_SIZE in 20 25 26 27 28 29 30 31 32
+do
+    for NUM_CORE in 0 1 2 4 6 8 16 32 64
+    do
 	eval "./tbb_test -n$NUM_CORE -e$EX_SIZE"
-	let NUM_CORE=NUM_CORE*2
     done
-    let EX_SIZE=EX_SIZE+1
 done
