8274546529274e7024c9f78a8bb50a4e3d7b0cee
Victor Nicolet <victor.nicolet@polytechnique.edu>
2018-01-11 15:55:37 -0500

    Correction in parser.

--- ocamllibs/test/TestSynthlib.ml
+++ ocamllibs/test/TestSynthlib.ml
@@ -3,22 +3,25 @@ open Format
 open Synthlib
 open Utils.PpTools;;
 
-printf "%s********* TESTING SYNTHLIB2 PARSER **********%s@." (color "b-blue") color_default;;
-
-let inputs = Conf.project_dir^"/ocamllibs/test/synthlib/";;
-
 let on_input input =
   printf "%sInput: %s%s@." (color "green") input color_default;
-  let parsed =
-    try
+  try
+    let _ =
       parsechan (open_in (project_dir^"/ocamllibs/test/synthlib/"^input))
-    with Syparser.Error as e ->
-      (print_err_std "Failed to parse input\n"; raise e)
-  in
-  printsy parsed;;
+    in
+    printf "%s%sOK%s@." (color "black") (color "b-green") color_default
+  with Syparser.Error->
+    print_err_std "Failed to parse input.\n"
 
-let children = Sys.readdir inputs in
-let filter s = (not (String.contains s '~'))
-               && (not (String.contains s '*'))
-               && (not (String.contains s '#')) in
-Array.iter  (fun s -> if filter s then on_input s) children;;
+
+
+
+let test () =
+  printf "%s********* TESTING SYNTHLIB2 PARSER **********%s@."
+    (color "b-blue")
+    color_default;
+  let children = Sys.readdir (Conf.project_dir^"/ocamllibs/test/synthlib/") in
+  let filter s = (not (String.contains s '~'))
+                 && (not (String.contains s '*'))
+                 && (not (String.contains s '#')) in
+  Array.iter  (fun s -> if filter s then on_input s) children
