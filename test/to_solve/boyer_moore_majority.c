c031abd0bd0dedc24f3ddc18c9526df40cd52a01
Victor Nicolet <victor.nicolet@polytechnique.edu>
2018-01-31 13:12:03 -0500

    Adding new example.

---
+++ test/to_solve/boyer_moore_majority.c
@@ -0,0 +1,21 @@
+int majority (int *a, int n){
+
+  int cnt = 0;
+  int i = 0;
+  int m = 0;
+
+  for(i = 0; i < n; i++){
+    if(cnt == 0) {
+      m = a[i];
+      cnt = 1;
+    } else {
+      if(m == a[i]) {
+	cnt ++;
+      } else {
+	cnt --;
+      }
+    }
+  }
+
+  return m;
+}
