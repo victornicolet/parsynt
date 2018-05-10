af9635fb0bb89b6f7dc98ef90d8900554af966eb
Victor Nicolet <victorn@cs.toronto.edu>
2018-05-10 17:09:08 -0400

    Adding example: max top rectangle.

---
+++ examples/rosette/mtr_join.el
@@ -0,0 +1,19 @@
+;; This buffer is for text that is not saved, and for Lisp evaluation.
+;; To create a file, visit it with C-x C-f and enter text in its buffer.
+
+(let ([_fs_3
+	   (LoopFunc 0
+				 (lambda (j) (< j $_end))
+				 (lambda (j) (add1 j))
+				 (() )
+               (lambda (__s7 j) (let ()
+                                   (let ([c c][r_mts r_mts][r_mss r_mss])
+                                      (let ([c (list-set c j (+
+                                                              (list-ref r.c j)
+                                                              (+
+                                                               (list-ref c j)
+                                                               -1)))])
+                                         ($Vi_ii c (+ r_mts (list-ref c j))
+                                           (max (+ (list-ref c j) r_mts)
+                                            (max r_mss -285))))))))])
+   ($Vi_ii c r_mts r_mss))
