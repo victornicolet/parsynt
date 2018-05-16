6f5ddefc111069880bb1ba902b72e6a77ed2eab2
Victor Nicolet <victorn@cs.toronto.edu>
2018-05-16 15:24:47 -0400

    Adding max bottom left rectangle example.

---
+++ test/nested_loops/mblr.c
@@ -0,0 +1,21 @@
+
+int m_bot_left_rectangle(int** A, int n, int m) {
+  int line_sum = 0;
+  int* max_rect_col;
+  int mblr = 0; // The max bottom left rectangle
+
+  max_rect_col = malloc(sizeof(max_rect_col) * m);
+
+  for(int i = 0; i < n; i++)
+	{
+	  line_sum = 0;
+	  for(int j = 0; j < m; j++)
+		{
+		  line_sum += A[i][j];
+		  max_rect_col[j] = max(max_rect_col[j] + line_sum, 0);
+		  mblr = max(mblr, max_rect_col[j]);
+		}
+	}
+
+  return mblr;
+}
