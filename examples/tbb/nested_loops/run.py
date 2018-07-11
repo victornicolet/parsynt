47ded45cdfb2ee61233e354cdd23f6004687877e
Victor Nicolet <victorn@mail.cs.toronto.edu>
2018-07-11 09:53:41 -0400

    Experiment Sorted2D done.

--- examples/tbb/nested_loops/run.py
+++ examples/tbb/nested_loops/run.py
@@ -3,16 +3,16 @@ from subprocess import call
 experiments = [ 'Exp2DSorted',
                 'Exp2DSum',
                 'ExpGradient1',
-                'ExpGradient2',
+                # 'ExpGradient2',
                 'ExpMinMax',
                 'ExpMaxDist',
                 'ExpMinMaxCol',
                 'ExpMaxLeftRect',
                 'ExpMaxTopStrip',
-                'ExpMaxBottomStrip',
+                # 'ExpMaxBottomStrip',
                 'ExpMaxSegStrip',
                 'ExpMaxTopBox',
-                'ExpMaxBottomBox',
+                # 'ExpMaxBottomBox',
                 'ExpMaxSegBox',
                 'ExpMTLR',
                 'ExpMTRR',
@@ -20,10 +20,10 @@ experiments = [ 'Exp2DSorted',
                 'ExpMode',
                 'ExpOverlapping']
 
-num_cores = [1, 2, 3, 4, 6, 8, 10, 12, 16, 20, 24, 32]
+num_cores = [1, 2, 3, 4, 6, 8, 10, 12, 16, 24, 32, 46, 58, 64]
 
 
-for nc in num_cores:
-    for expr in experiments:
-        with open('explog.csv', 'a+') as myoutfile:
+for expr in experiments:
+    for nc in num_cores:
+        with open('explog_compsbk2.csv', 'a+') as myoutfile:
             call(["./" + expr, str(nc)], stdout=myoutfile)
