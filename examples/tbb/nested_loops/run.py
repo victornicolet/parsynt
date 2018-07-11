7eef925e7d3295091de656ea92e937d2fc4a4bf0
Victor Nicolet <victorn@cs.toronto.edu>
2018-07-11 08:48:00 -0400

    Experiments.

--- examples/tbb/nested_loops/run.py
+++ examples/tbb/nested_loops/run.py
@@ -2,7 +2,6 @@ from subprocess import call
 
 experiments = [ 'Exp2DSorted',
                 'Exp2DSum',
-                'Exp2DSorted',
                 'ExpGradient1',
                 'ExpGradient2',
                 'ExpMinMax',
@@ -26,4 +25,5 @@ num_cores = [1, 2, 3, 4, 6, 8, 10, 12, 16, 20, 24, 32]
 
 for nc in num_cores:
     for expr in experiments:
-        call(["./" + expr, str(nc), ">> explog.csv"])
+        with open('explog.csv', 'a+') as myoutfile:
+            call(["./" + expr, str(nc)], stdout=myoutfile)
