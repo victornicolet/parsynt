bdefb9062bc570fa278b1b742b1e9178f782b8d7
Victor Nicolet <victorn@mail.cs.toronto.edu>
2018-07-11 23:44:47 -0400

    SaddlePoint

--- examples/tbb/nested_loops/run.py
+++ examples/tbb/nested_loops/run.py
@@ -1,24 +1,24 @@
 from subprocess import call
 
-experiments = [ 'Exp2DSorted',
-                'Exp2DSum',
-                'ExpGradient1',
-                # 'ExpGradient2',
-                'ExpMinMax',
-                'ExpMaxDist',
-                'ExpMinMaxCol',
-                'ExpMaxLeftRect',
-                'ExpMaxTopStrip',
-                # 'ExpMaxBottomStrip',
-                'ExpMaxSegStrip',
-                'ExpMaxTopBox',
-                # 'ExpMaxBottomBox',
-                'ExpMaxSegBox',
-                'ExpMTLR',
-                'ExpMTRR',
-                'ExpSaddlePoint',
-                'ExpMode',
-                'ExpOverlapping']
+experiments = [ # 'Exp2DSorted',  
+    # 'Exp2DSum',
+    # 'ExpGradient1',
+    # 'ExpGradient2',
+    # 'ExpMinMax',
+    # 'ExpMaxDist',
+    # 'ExpMinMaxCol',
+    # 'ExpMaxLeftRect',
+    # 'ExpMaxTopStrip',
+    # 'ExpMaxBottomStrip',
+    # 'ExpMaxSegStrip',
+    # 'ExpMaxTopBox',
+    # 'ExpMaxBottomBox',
+    # 'ExpMaxSegBox',
+    'ExpMTLR',
+    'ExpMTRR',
+    'ExpSaddlePoint',
+    'ExpMode',
+    'ExpOverlapping']
 
 num_cores = [1, 2, 3, 4, 6, 8, 10, 12, 16, 24, 32, 46, 58, 64]
 
