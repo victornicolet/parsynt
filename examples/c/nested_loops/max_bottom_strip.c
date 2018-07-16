de7c0b0e985c6774966b931e64b51f92533aa417
Victor Nicolet <victorn@cs.toronto.edu>
2018-07-16 11:14:55 -0400

    Post experiment cleanup.

--- examples/c/nested_loops/max_bottom_strip.c
+++ examples/c/nested_loops/max_bottom_strip.c
@@ -1,6 +1,13 @@
+/*
+   Computes the sum of the bottom subarray of an array whose
+   sum is maximal.
+   (example of the maximum bottom strip of an array).
+ */
+
 int _mbotstrip(int **a, int m, int n){
   int max_bot_strip = 0;
   int strip_sum = 0;
+  /* Auxiliary : sum = 0; */
   for(int i = 0; i < n; i++)
 	{
 	  strip_sum = 0;
@@ -8,7 +15,15 @@ int _mbotstrip(int **a, int m, int n){
 		{
 		  strip_sum += a[i][j];
 		}
+	  /*  Auxiliary : sum += strip_sum */
 	  max_bot_strip = max(max_bot_strip + strip_sum, 0);
 	}
   return max_bot_strip;
 }
+
+/*
+   Join:
+   strip_sum = r.strip_sum
+   sum = l.sum + r.sum
+   max_bot_strip = max(r.max_bot_strip, r.sum + l.max_bot_strip);
+ */
--- examples/c/nested_loops/max_top_strip.c
+++ examples/c/nested_loops/max_top_strip.c
@@ -1,3 +1,9 @@
+/*
+   Computes the sum of the top subarray of an array whose
+   sum is maximal.
+   (example of the maximum top strip of an array).
+ */
+
 int _mtopstrip(int **a, int m, int n){
   int top_strip_sum = 0;
   int max_top_strip = 0;
@@ -12,3 +18,11 @@ int _mtopstrip(int **a, int m, int n){
 	}
   return max_top_strip;
 }
+
+
+/*
+  Join :
+  top_strip_sum = r.top_strip_sum + l.top_strip_sum
+  max_top_strip = max(l.max_top_strip, r.max_top_strip + l.top_strip_sum)
+
+*/
